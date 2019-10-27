168101c93ce76d112e3859bfb7a7bdae092523f0
Lincoln N Araujo <lincoln.n.araujo@gmail.com>
2019-10-27 17:27:44 -0300

    Endpoins de Usuario
    Foi adicionado a configuração para retorno de requisições paginadas (Pageable), no endpoint de Usuario

--- src/main/java/br/com/desafio/desafiospring/controller/UsuarioCotroller.java
+++ src/main/java/br/com/desafio/desafiospring/controller/UsuarioCotroller.java
@@ -1,22 +1,37 @@
 package br.com.desafio.desafiospring.controller;
 
+import br.com.desafio.desafiospring.model.Usuario;
+import br.com.desafio.desafiospring.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * UsuarioCotroller
- * Author: Lincoln Araujo
+ * UsuarioCotroller Author: Lincoln Araujo
  */
-
 
 @RestController
 @RequestMapping("/usuario")
-public class UsuarioCotroller 
-{
+public class UsuarioCotroller {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
     @GetMapping("/info")
-    public String index(){
+    public String index() {
         return "Usuário - Controller";
     }
-    
+
+    @GetMapping("/lista")
+    public Page<Usuario> lista(
+        @PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 5) Pageable paginacao) {
+        Page<Usuario> usuarios = usuarioRepository.findAll(paginacao);
+        return usuarios;
+    }
+
 }
--- src/main/java/br/com/desafio/desafiospring/repository/UsuarioRepository.java
+++ src/main/java/br/com/desafio/desafiospring/repository/UsuarioRepository.java
@@ -5,14 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
+
 /**
  * UsuarioRepository Author: Lincoln Araujo
  */
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>
 {
-    @Override
-    List<Usuario> findAll();
-
     List<Usuario> findAllBySexoIgnoreCase(String _sexo);
 
     List<Usuario> findAllByStatusIgnoreCase(String _status);
