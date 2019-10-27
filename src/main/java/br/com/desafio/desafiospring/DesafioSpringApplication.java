5f671f3f657c6a734fcf0ac6971554101979e6d7
Lincoln N Araujo <lincoln.n.araujo@gmail.com>
2019-10-27 18:06:49 -0300

    Controle de Cache
    Adicionado controle (criação e deleção) de cache no endpoint de usuario

--- src/main/java/br/com/desafio/desafiospring/DesafioSpringApplication.java
+++ src/main/java/br/com/desafio/desafiospring/DesafioSpringApplication.java
@@ -2,10 +2,12 @@ package br.com.desafio.desafiospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableCaching
 public class DesafioSpringApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/desafio/desafiospring/controller/UsuarioCotroller.java
+++ src/main/java/br/com/desafio/desafiospring/controller/UsuarioCotroller.java
@@ -2,14 +2,26 @@ package br.com.desafio.desafiospring.controller;
 
 import br.com.desafio.desafiospring.model.Usuario;
 import br.com.desafio.desafiospring.repository.UsuarioRepository;
+import java.net.URI;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+
+
 
 /**
  * UsuarioCotroller Author: Lincoln Araujo
@@ -28,10 +40,21 @@ public class UsuarioCotroller {
     }
 
     @GetMapping("/lista")
+    @Cacheable(value = "listaDeUsuarios")
     public Page<Usuario> lista(
         @PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 5) Pageable paginacao) {
         Page<Usuario> usuarios = usuarioRepository.findAll(paginacao);
         return usuarios;
+    }
+
+    @PostMapping
+    @Transactional
+    @CacheEvict(value = "listaDeUsuarios", allEntries = true)
+    public ResponseEntity<Usuario> cadastrar(@RequestBody @Valid Usuario novo, UriComponentsBuilder uriBuilder)
+    {
+        usuarioRepository.save(novo);
+        URI uri = uriBuilder.path("/usuarios/{id}").buildAndExpand(novo.getId()).toUri();
+        return ResponseEntity.created(uri).body(novo);
     }
 
 }
